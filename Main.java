0e505b4a0e6cdd123391a35b4fdc009a70959eea
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-13 16:03:55 +0000

    A method takes integers tells factors

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 /**
- *
- * @author 
+ * A method that takes an integer and tells the user its factors
+ * @author Liana Bazzarella
  */
 public class Main {
+
+ // a method that takes an integer and tells the user its factors
+  public static void factors(int integer){
+   for(int i = 1; i <= integer; i++){
+     if(integer%i == 0){
+       System.out.println(i);
+     }
+   }
+ }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // prints the factors out to the user
+    factors(30);
   }
 }
